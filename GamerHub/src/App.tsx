cb9d189e4f364673be42d00a9fab8b4a1df41332
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-03-13 23:00:56 -0400

    Init

---
+++ GamerHub/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from './Message';
+
+function App(){
+  return (
+  <div>
+    <Message />
+  </div>
+  )
+}
+
+export default App;
---
+++ GamerHub/src/Message.tsx
@@ -0,0 +1,11 @@
+
+const Message = () => {
+  
+    const name = 'John Doe'
+  
+    return (
+    <h1>Hello {name}</h1>
+  )
+}
+
+export default Message;
